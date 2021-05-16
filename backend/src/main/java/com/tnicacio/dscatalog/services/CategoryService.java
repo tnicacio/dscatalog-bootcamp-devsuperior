31fcbc8b17a3f27c884d947113f4085d01e681aa
tnicacio <nicacio.t92@gmail.com>
2021-05-16 13:16:03 -0300

    Delete Category

--- backend/src/main/java/com/tnicacio/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/tnicacio/dscatalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.tnicacio.dscatalog.dto.CategoryDTO;
 import com.tnicacio.dscatalog.entities.Category;
 import com.tnicacio.dscatalog.repositories.CategoryRepository;
+import com.tnicacio.dscatalog.services.exceptions.DatabaseException;
 import com.tnicacio.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -55,6 +58,16 @@ public class CategoryService {
 			return new CategoryDTO(entity);
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);					
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
 		}
 	}
 	
