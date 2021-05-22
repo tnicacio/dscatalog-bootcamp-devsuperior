4327095db0e12658f790df74dd3f92543c4d9162
tnicacio <nicacio.t92@gmail.com>
2021-05-22 09:40:25 -0300

    testing product repository

---
+++ backend/src/test/java/com/tnicacio/dscatalog/tests/Factory.java
@@ -0,0 +1,27 @@
+package com.tnicacio.dscatalog.tests;
+
+import java.time.Instant;
+
+import com.tnicacio.dscatalog.dto.ProductDTO;
+import com.tnicacio.dscatalog.entities.Category;
+import com.tnicacio.dscatalog.entities.Product;
+
+public class Factory {
+	
+	public static Product createProduct() {
+		Product product = new Product(1L,
+				"Phone",
+				"Good Phone",
+				800.0,
+				"https://img.com/img.png",
+				Instant.parse("2020-10-20T03:00:00Z"));
+		product.getCategories().add(new Category(2L, "Electronics"));
+		return product;
+	}
+	
+	public static ProductDTO createProductDTO() {
+		Product product = createProduct();
+		return new ProductDTO(product, product.getCategories());
+	}
+
+}
