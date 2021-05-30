6ebec31bf66b57dc0fb5bef90c44517f08f1900e
tnicacio <nicacio.t92@gmail.com>
2021-05-30 10:15:43 -0300

    Adding Role Entity

---
+++ backend/src/main/java/com/tnicacio/dscatalog/entities/Role.java
@@ -0,0 +1,65 @@
+package com.tnicacio.dscatalog.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Role implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String authoriry;
+	
+	public Role() {}
+
+	public Role(Long id, String authoriry) {
+		this.id = id;
+		this.authoriry = authoriry;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAuthoriry() {
+		return authoriry;
+	}
+
+	public void setAuthoriry(String authoriry) {
+		this.authoriry = authoriry;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+}
