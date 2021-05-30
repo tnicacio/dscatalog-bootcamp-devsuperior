f006c423ee530b7f9bb5f2dfa22ca024ce74d2ab
tnicacio <nicacio.t92@gmail.com>
2021-05-30 10:30:12 -0300

    ORM mapping and database seeding

--- backend/src/main/java/com/tnicacio/dscatalog/entities/User.java
+++ backend/src/main/java/com/tnicacio/dscatalog/entities/User.java
@@ -4,10 +4,17 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
@@ -16,8 +23,13 @@ public class User implements Serializable{
 	private Long id;
 	private String firstName;
 	private String lastName;
+	private String email;
 	private String password;
 	
+	@ManyToMany
+	@JoinTable(name = "tb_user_role",
+		joinColumns = @JoinColumn(name = "user_id"),
+		inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
 	
 	public User() {}
@@ -51,6 +63,14 @@ public class User implements Serializable{
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public String getPassword() {
