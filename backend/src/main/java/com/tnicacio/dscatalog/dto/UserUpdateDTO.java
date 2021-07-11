a7ae36de0aa7d97b2d7c9d641a4c030bf1c72fb4
tnicacio <nicacio.t92@gmail.com>
2021-07-11 09:34:58 -0300

    dscatalog from chapter5

--- backend/src/main/java/com/tnicacio/dscatalog/dto/UserUpdateDTO.java
+++
@@ -1,9 +0,0 @@
-package com.tnicacio.dscatalog.dto;
-
-import com.tnicacio.dscatalog.services.validation.UserUpdateValid;
-
-@UserUpdateValid
-public class UserUpdateDTO extends UserDTO {
-	private static final long serialVersionUID = 1L;
-	
-}
--- backend/src/main/java/com/tnicacio/dscatalog/repositories/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tnicacio.dscatalog.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tnicacio.dscatalog.entities.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long>{
-
-}
